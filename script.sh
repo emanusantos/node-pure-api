2835f2ff58d28329240de4eb1904106ea15b5e12
emanusantos <emanuel.lmds1@gmail.com>
2022-08-13 01:52:37 -0300

    feat: api

---
+++ script.sh
@@ -0,0 +1,5 @@
+echo 'requesting all heroes'
+curl localhost:3000/heroes
+
+echo 'requesting foo'
+curl localhost:3000/heroes/1
--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ const http = require("http");
 
 const PORT = 3000;
 
-const handler = (request, response) => {};
+const DEFAULT_HEADER = { "Content-Type": "application/json" };
+
+const routes = {
+  "/heroes:get": async (request, response) => {
+    const { id } = request.queryString;
+    console.log({ id });
+    response.end();
+  },
+
+  default: (request, response) => {
+    response.write("Hello!");
+    response.end();
+  },
+};
+
+const handler = (request, response) => {
+  const { url, method } = request;
+
+  const [, route, id] = url.split("/");
+
+  request.queryString = { id: isNaN(id) ? id : Number(id) };
+
+  const key = `/${route}:${method.toLowerCase()}`;
+
+  response.writeHead(200, DEFAULT_HEADER);
+
+  const chosen = routes[key] || routes.default;
+  return chosen(request, response);
+};
 
 http.createServer(handler).listen(PORT, function () {
   console.log("server running at", PORT);
