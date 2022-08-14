8351bfdd0cf6d315e69a9671f0e3fcbdf9160be5
emanusantos <emanuel.lmds1@gmail.com>
2022-08-14 17:17:43 -0300

    finished api

--- script.sh
+++ script.sh
@@ -1,5 +1,16 @@
-echo 'requesting all heroes'
+echo '\n\n requesting all heroes'
 curl localhost:3000/heroes
 
-echo 'requesting foo'
+echo '\n\n requesting foo'
 curl localhost:3000/heroes/1
+
+echo '\n\n requesting with wrong body'
+curl --silent -X POST \
+    --data-binary '{"invalid": "data"}' \
+    localhost:3000/heroes
+
+echo '\n\n creating emanu'
+CREATE=$(curl --silent -X POST \
+    --data-binary '{"name": "Emanu", "age": 21, "power": "code" }' \
+    localhost:3000/heroes)
+echo $CREATE
